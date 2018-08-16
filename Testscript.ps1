a7729a34ad472448d6a3dc8dafdb7edd6919e0b8
ghaffarkhan <42437180+ghaffarkhan@users.noreply.github.com>
2018-08-16 12:13:53 +0100

    Add files via upload

---
+++ Testscript.ps1
@@ -0,0 +1,6 @@
+﻿"This is a test file write output"  | out-file c:\temp\test.txt -force
+
+$temp = (gci env:Temp).value + "\test2.txt"
+"This is test number 2" | out-file $temp 
+
+[System.Windows.MessageBox]::Show('Would  you like to play a game?','Game input','YesNoCancel','Error')
